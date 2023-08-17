a9ac8865f0c001a23e3e75232a03956851d3206b
IviKeiko <jooly.klee@gmail.com>
2023-08-17 11:23:40 +0200

    getting prompts from DB and displaying on the feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,55 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => {
+        return (
+          <PromptCard
+            key={prompt._id}
+            prompt={prompt}
+            handleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
 const Feed = () => {
-  return <div>Im feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPrompts(data);
+    };
+    fetchPrompts();
+  }, []);
+
+  console.log(prompts);
+
+  const handleSearchChange = (e) => {};
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 export default Feed;
